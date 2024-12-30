9f58ffbd71986b37466d316e771e7e58cd7d6a38
Achraf Meziouni <amezioun@c2r4p2.1337.ma>
2024-12-30 11:42:38 +0100

    checking the map walls, considering spaces true

--- cmake-build-debug/CMakeFiles/CUB3D.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/CUB3D.dir/DependInfo.cmake
@@ -1,24 +1,23 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/amezioun/Desktop/CUB3D/cub3.c" "/Users/amezioun/Desktop/CUB3D/cmake-build-debug/CMakeFiles/CUB3D.dir/cub3.c.o"
+  "/Users/amezioun/Desktop/CUB3D/getnextline/get_next_line.c" "/Users/amezioun/Desktop/CUB3D/cmake-build-debug/CMakeFiles/CUB3D.dir/getnextline/get_next_line.c.o"
+  "/Users/amezioun/Desktop/CUB3D/getnextline/get_next_line_utils.c" "/Users/amezioun/Desktop/CUB3D/cmake-build-debug/CMakeFiles/CUB3D.dir/getnextline/get_next_line_utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../getnextline"
   )
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/captain/Desktop/CUB3D/cub3.c" "CMakeFiles/CUB3D.dir/cub3.c.o" "gcc" "CMakeFiles/CUB3D.dir/cub3.c.o.d"
-  "/home/captain/Desktop/CUB3D/getnextline/get_next_line.c" "CMakeFiles/CUB3D.dir/getnextline/get_next_line.c.o" "gcc" "CMakeFiles/CUB3D.dir/getnextline/get_next_line.c.o.d"
-  "/home/captain/Desktop/CUB3D/getnextline/get_next_line_utils.c" "CMakeFiles/CUB3D.dir/getnextline/get_next_line_utils.c.o" "gcc" "CMakeFiles/CUB3D.dir/getnextline/get_next_line_utils.c.o.d"
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- cub3.c
+++ cub3.c
@@ -869,20 +869,48 @@ void check_filled_map(t_map *map)
         ft_exit("Error: Map not filled correctly!\n", map);
 }
 
+
+void check_ones(char *line)
+{
+    while(*line)
+    {
+        if(*line != '1' && *line != ' ')
+        {
+            printf("Error: first or second line '%c'\n", *line);
+            exit(1);
+        }
+        line++;
+    }
+}
+
 int check_map_chars(char **map){
     int i = 0;
     int j = 0;
     int player_count = 0;
-    // int end = 0;
+    int index = 0;
+     int end = 0;
+     check_ones(map[0]);
     while (map[i]) {
-        printf("%s\n", map[i]);
-        // end = strlen(map[i]);
-        // if(map[i][end] != '1')
-        // {
-        //     printf("map should closed by ones '1, %c", map[i][end]);
-        //     exit(1);
-        // }
+//        printf("%s\n", map[i]);
+        end = strlen(map[i])- 1;
+        if(map[i][end] != '1' && map[i][end] != ' ')
+        {
+            printf("Error: Last character in the map\n");
+            exit(1);
+        }
+        printf("end: %c\n", map[i][end]);
+        while(map[i][j] == ' ' || map[i][j] == '\t')
+        {
+//                printf("char: %d: '%c'", map[i][j], j);
+                j++;
+        }
+        index = j;
+        if(map[i][index] != '1'){
+            printf("\nError: first character in the map '%c'\n", map[i][index]);
+            exit(1);
+        }
         while (map[i][j]) {
+//            printf("char in while %c", map[i][j]);
             if (map[i][j] != '1' && map[i][j] != '0' && map[i][j] != ' ' && map[i][j] != 'N' && map[i][j] != 'S' && map[i][j] != 'W' && map[i][j] != 'E') {
                 printf("Error: %c", map[i][j]);
                 exit(1);
@@ -900,6 +928,7 @@ int check_map_chars(char **map){
         printf("the map need one player\n");
         exit(1);
     }
+    check_ones(map[i - 1]);
     return 0;
 }
 
