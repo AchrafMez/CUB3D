7c374cc9584a1db4a23cbd85e8d418db79d74546
Abdelali <abde69436@gmail.com>
2025-01-11 20:54:04 +0100

    render 3d projection walls

--- raycasting/raycast.c
+++ raycasting/raycast.c
@@ -6,19 +6,22 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 11:31:49 by abmahfou          #+#    #+#             */
-/*   Updated: 2025/01/10 17:55:50 by abmahfou         ###   ########.fr       */
+/*   Updated: 2025/01/11 20:52:34 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3.h"
 #include "/Users/abmahfou/MLX42/include/MLX42/MLX42.h"
 
-#define TILE_SIZE 32
-#define COLOR_RED 0xFF0000FF
+#define TILE_SIZE 64
+#define COLOR_GREEN 0x0F000FF
+#define COLOR_RED 0x7FFFFFF0
 #define COLOR_WALL 0x6468699B
 #define COLOR_SPACE 0x37373737
 #define PI 3.14159265358979323846
+#define WALL_STRIP_WIDTH 1
 #define LINE_LENGTH 30
+#define MINIMAP_SCALE_FACTOR 0.2
 
 typedef struct	s_player {
 	float	x;
@@ -49,7 +52,8 @@ typedef struct	s_ray
 typedef struct	s_cub
 {
 	mlx_t		*mlx;
-	mlx_image_t	*image;
+	mlx_image_t	*mini_map;
+	mlx_image_t	*img;
 	int			win_width;
 	int			win_height;
 	char		**map;
@@ -80,7 +84,7 @@ void render_map(t_cub *game)
 			{
 				px = -1;
 				while (++px < TILE_SIZE)
-					mlx_put_pixel(game->image, x * TILE_SIZE + px, y * TILE_SIZE + py, color);
+					mlx_put_pixel(game->mini_map, (x * TILE_SIZE + px) * MINIMAP_SCALE_FACTOR, (y * TILE_SIZE + py) * MINIMAP_SCALE_FACTOR, color);
 			}
 		}
 	}
@@ -91,8 +95,12 @@ int	is_WALL(t_cub *game, int x, int y)
 	int	X_index;
 	int	Y_index;
 
-	Y_index = floor(y / TILE_SIZE);
-	X_index = floor(x / TILE_SIZE);
+	if (x > game->win_width)
+		x--;
+	if (y > game->win_height)
+		y--;
+	Y_index = round(y / TILE_SIZE);
+	X_index = round(x / TILE_SIZE);
 	if (game->map[Y_index][X_index] == '1')
 		return (1);
 	return (0);
@@ -151,7 +159,8 @@ void	clear_image(mlx_image_t *img)
 
 void	render_rays(t_cub *game, float x1, float y1)
 {
-	draw_line(game->player->ray, game->player->pl->instances->x + 4, game->player->pl->instances->y + 4, x1, y1, 0xFF0000FF);
+	draw_line(game->player->ray, game->player->pl->instances->x * MINIMAP_SCALE_FACTOR, game->player->pl->instances->y * MINIMAP_SCALE_FACTOR,
+		x1 * MINIMAP_SCALE_FACTOR, y1 * MINIMAP_SCALE_FACTOR, COLOR_RED);
 }
 
 double	distance_between_2_points(int32_t x1, int32_t y1, float x2, float y2)
@@ -200,13 +209,13 @@ void	cast_rays(t_ray *ray, t_cub *game)
 	next_horz_X = x_intercept;
 	next_horz_Y = y_intercept;
 
-	if (ray->ray_facing_up)
-		next_horz_Y--;
+	// if (ray->ray_facing_up)
+	// 	next_horz_Y--;
 
-	while (next_horz_X >= 0 && next_horz_X <= game->win_width * TILE_SIZE
-		&& next_horz_Y >= 0 && next_horz_Y <= game->win_height * TILE_SIZE)
+	while (next_horz_X >= 0 && next_horz_X <= game->win_width
+		&& next_horz_Y >= 0 && next_horz_Y <= game->win_height)
 	{
-		if (is_WALL(game, next_horz_X, next_horz_Y))
+		if (is_WALL(game, next_horz_X, next_horz_Y - (ray->ray_facing_up ? 1 : 0)))
 		{
 			found_horz_hit = true;
 			horz_wall_hit_X = next_horz_X;
@@ -254,12 +263,12 @@ void	cast_rays(t_ray *ray, t_cub *game)
 	next_vert_X = x_intercept;
 	next_vert_Y = y_intercept;
 
-	if (ray->ray_facing_left)
-		next_vert_X--;
+	// if (ray->ray_facing_left)
+	// 	next_vert_X--;
 
-	while (next_vert_X >= 0 && next_vert_X <= game->win_width * TILE_SIZE && next_vert_Y >= 0 && next_vert_Y <= game->win_height * TILE_SIZE)
+	while (next_vert_X >= 0 && next_vert_X <= game->win_width && next_vert_Y >= 0 && next_vert_Y <= game->win_height)
 	{
-		if (is_WALL(game, next_vert_X, next_vert_Y))
+		if (is_WALL(game, next_vert_X - (ray->ray_facing_left ? 1 : 0), next_vert_Y))
 		{
 			found_vert_hit = true;
 			vert_wall_hit_X = next_vert_X;
@@ -274,12 +283,14 @@ void	cast_rays(t_ray *ray, t_cub *game)
 	}
 
 	// Calculate the distance to get the smallest point
-	double	horz_hit_distance = 2342342342.0;
-	double	vert_hit_distance = 2342342342.0;
+	double	horz_hit_distance = DBL_MAX;
+	double	vert_hit_distance = DBL_MAX;
 	if (found_horz_hit == true)
-		horz_hit_distance = distance_between_2_points(game->player->pl->instances->x, game->player->pl->instances->y, horz_wall_hit_X, horz_wall_hit_Y);
+		horz_hit_distance = distance_between_2_points(game->player->pl->instances->x,
+			game->player->pl->instances->y, horz_wall_hit_X, horz_wall_hit_Y);
 	if (found_vert_hit == true)
-		vert_hit_distance = distance_between_2_points(game->player->pl->instances->x, game->player->pl->instances->y, vert_wall_hit_X, vert_wall_hit_Y);
+		vert_hit_distance = distance_between_2_points(game->player->pl->instances->x,
+			game->player->pl->instances->y, vert_wall_hit_X, vert_wall_hit_Y);
 	if (horz_hit_distance < vert_hit_distance)
 	{
 		ray->wall_hit_X = horz_wall_hit_X;
@@ -313,17 +324,18 @@ t_ray	*create_Ray(float angle)
 	return (ray);
 }
 
-void	cast_all_rays(t_cub *game)
+t_ray	**cast_all_rays(t_cub *game)
 {
 	int		columnId;
 	float	ray_angle;
 	int		i;
 	int		rays_num;
 
-	rays_num = (game->win_width * TILE_SIZE);
-	t_ray	*rays[rays_num];
+	rays_num = game->win_width;
+	t_ray	**rays;
 
 	clear_image(game->player->ray);
+	rays = malloc(sizeof(t_ray *) * rays_num);
 	columnId = 0;
 	ray_angle = game->player->rotation_angle - (game->player->FOV / 2);
 	i = -1;
@@ -336,6 +348,54 @@ void	cast_all_rays(t_cub *game)
 		rays[i] = ray;
 		ray_angle += game->player->FOV / rays_num;
 		columnId++;
+	}
+	return (rays);
+}
+
+void	draw_rectangle(mlx_image_t *img, int x, int y, int width, int height, uint32_t color)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < height)
+	{
+		j = 0;
+		while (j < width)
+		{
+			if (x + j >= 0 && (uint32_t)(x + j) < img->width && (uint32_t)(y + i) >= 0 && (uint32_t)(y + i) < img->height)
+            {
+                mlx_put_pixel(img, x + j, y + i, color);
+            }
+			j++;
+		}
+		i++;
+	}
+}
+
+void	render_3d_projection_walls(t_ray **rays, t_cub *game)
+{
+	int		i;
+	int		rays_num;
+	t_ray	*ray;
+	int		wall_strip_height;
+	float	ray_distance;
+	float	distance_projection_plane;
+
+	i = -1;
+	clear_image(game->img);
+	rays_num = game->win_width;
+	while (++i < rays_num)
+	{
+		ray = rays[i];
+		ray_distance = ray->distance;
+		distance_projection_plane = (game->win_width / 2) / tan(game->player->FOV / 2);
+		wall_strip_height = (TILE_SIZE / ray_distance) * distance_projection_plane;
+		draw_rectangle(game->img, i * WALL_STRIP_WIDTH,
+						(game->win_height / 2) - (wall_strip_height / 2),
+						WALL_STRIP_WIDTH,
+						wall_strip_height,
+						COLOR_RED);
 	}
 }
 
@@ -351,10 +411,10 @@ void update_line(t_cub *game) {
 
 int	is_collision(t_cub *game, float new_X, float new_Y)
 {
-	if (is_WALL(game, new_X - 8, new_Y - 8) ||
-		is_WALL(game, new_X + 8, new_Y - 8) ||
-		is_WALL(game, new_X - 8, new_Y + 8) ||
-		is_WALL(game, new_X + 8, new_Y + 8))
+	if (is_WALL(game, new_X - TILE_SIZE / 4, new_Y - TILE_SIZE / 4) ||
+		is_WALL(game, new_X + TILE_SIZE / 4, new_Y - TILE_SIZE / 4) ||
+		is_WALL(game, new_X - TILE_SIZE / 4, new_Y + TILE_SIZE / 4) ||
+		is_WALL(game, new_X + TILE_SIZE / 4, new_Y + TILE_SIZE / 4))
 		return (1);
 	return (0);
 }
@@ -379,6 +439,7 @@ void	update_player(t_cub *game) {
 void	render(void *param)
 {
 	t_cub	*game;
+	t_ray	**rays;
 
 	game = (t_cub *)param;
 	
@@ -397,7 +458,8 @@ void	render(void *param)
 	else
 		game->player->turn_direction = 0;
 	update_player(game);
-	cast_all_rays(game);
+	rays = cast_all_rays(game);
+	render_3d_projection_walls(rays, game);
 }
 
 void	player_pos(t_player *player, char **map)
@@ -460,31 +522,35 @@ int	main(void)
 	while (game.map[game.win_height])
 		game.win_height++;
 
-	game.mlx = mlx_init(game.win_width * TILE_SIZE, game.win_height * TILE_SIZE, "cub3D", true);
+	game.win_width *= TILE_SIZE;
+	game.win_height *= TILE_SIZE;
+	game.mlx = mlx_init(game.win_width, game.win_height, "cub3D", true);
 	if (!game.mlx) {
-		fprintf(stderr, "Failed to initialize MLX42\n");
+		fprintf(stderr, "Failed to initialize MLX\n");
 		return EXIT_FAILURE;
 	}
 
-	game.image = mlx_new_image(game.mlx, game.win_width * TILE_SIZE, game.win_height * TILE_SIZE);
+	game.mini_map = mlx_new_image(game.mlx, game.win_width, game.win_height);
 	pl.pl = mlx_new_image(game.mlx, TILE_SIZE, TILE_SIZE);
+	game.img = mlx_new_image(game.mlx, game.win_width, game.win_height);
 	game.player = &pl;
-	mlx_image_to_window(game.mlx, game.image, 0, 0);
+	mlx_image_to_window(game.mlx, game.mini_map, 0, 0);
+	mlx_image_to_window(game.mlx, game.img, 0, 0);
 	render_map(&game);
-	mlx_image_to_window(game.mlx, pl.pl, pl.x + TILE_SIZE / 3, pl.y + TILE_SIZE / 3);
-	for (int y = 0; y < 8; y++) {
-		for (int x = 0; x < 8; x++) {
+	mlx_image_to_window(game.mlx, pl.pl, (pl.x + TILE_SIZE / 3), (pl.y + TILE_SIZE / 3));
+	 /* for (int y = 0; y < (TILE_SIZE / 4); y++) {
+		for (int x = 0; x < (TILE_SIZE / 4); x++) {
 			mlx_put_pixel(pl.pl, x, y, COLOR_RED);
 		}
-	}
-	pl.line = mlx_new_image(game.mlx, game.win_width * TILE_SIZE, game.win_height * TILE_SIZE);
+	}  */
+	pl.line = mlx_new_image(game.mlx, game.win_width * MINIMAP_SCALE_FACTOR, game.win_height * MINIMAP_SCALE_FACTOR);
 	mlx_image_to_window(game.mlx, pl.line, 0, 0);
-	pl.ray = mlx_new_image(game.mlx, game.win_width * TILE_SIZE, game.win_height * TILE_SIZE);
+	pl.ray = mlx_new_image(game.mlx, game.win_width * MINIMAP_SCALE_FACTOR, game.win_height * MINIMAP_SCALE_FACTOR);
 	mlx_image_to_window(game.mlx, pl.ray, 0, 0);
 
 	mlx_loop_hook(game.mlx, render, &game);
 	mlx_loop(game.mlx);
-	mlx_delete_image(game.mlx, game.image);
+	mlx_delete_image(game.mlx, game.mini_map);
 	mlx_delete_image(game.mlx, pl.line);
 	mlx_terminate(game.mlx);
 	return EXIT_SUCCESS;
