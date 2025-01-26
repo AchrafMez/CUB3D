7a148e6cb638a3d2bde66f35ebc3257b5d42e9e1
Abdelali <abde69436@gmail.com>
2025-01-26 17:03:50 +0100

    done!

--- mandatory/cub3.c
+++ mandatory/cub3.c
@@ -6,16 +6,22 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 06:05:10 by amezioun          #+#    #+#             */
-/*   Updated: 2025/01/25 11:53:24 by abmahfou         ###   ########.fr       */
+/*   Updated: 2025/01/26 15:46:58 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3.h"
 
-void	leak(void)
+void	_run(t_map *map, char **av)
 {
-	system("leaks cub3D");
+	null_init(map);
+	read_map(av[1], &map);
+	check_filled_map(map);
+	check_map_chars(map);
+	whxy(&map);
+	check_map_spaces(map);
 }
+
 int	main(int ac, char **av)
 {
 	t_data	data;
@@ -28,20 +34,14 @@ int	main(int ac, char **av)
 			map = malloc(sizeof(t_map));
 			if (!map)
 				return (free_map(map), EXIT_FAILURE);
-			null_init(map);
-			read_map(av[1], &map);
-			check_filled_map(map);
-			check_map_chars(map);
-			whxy(&map);
-			check_map_spaces(map);
 			data.map = map;
+			_run(map, av);
 			if (raycast(&data))
 				return (free_map(map), EXIT_FAILURE);
 			free_map(map);
 		}
 		else
 			printf("Uncorrect filename\n");
-		// atexit(leak);
 	}
 	else
 		printf("Usage: cub3D *.cub\n");
